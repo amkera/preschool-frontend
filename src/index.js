0e1c189f01c0b5187972baadaaa059ae010d6d69
amkera <60901491+amkera@users.noreply.github.com>
2020-11-15 14:01:32 -0500

    import statements at the top of index.js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux';
+
+//set up the store => import create store from redux. it comes from redux.
+//import createStore from redux for async requests
+//import provider, anything we wrap in provider will have access to the redux store
+
 import App from './App';
 
 ReactDOM.render(
@@ -8,6 +16,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
