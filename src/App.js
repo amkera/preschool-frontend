0f3ed8b338231bab1379b91c0c9997362fdd2876
amkera <60901491+amkera@users.noreply.github.com>
2020-11-15 13:57:55 -0500

    ran npm install redux --save , npm install react-redux --save, npm install redux-thunk --save, npm install react-router-dom --save

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class App extends React.Component {
+
+  //test
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/students/2/payments')
+    .then(response => response.json())
+    .then(jsonObject => console.log(jsonObject))
+  }
+  //I have no access to jsonObject.payments if the serializers are not set up.
+
   render() {
     return (
       <div className="App">
