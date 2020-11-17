53e98b978a6d36cd9d3945479222f8c5b020e26f
amkera <60901491+amkera@users.noreply.github.com>
2020-11-17 11:37:50 -0500

    controlled form and onChange handler that is abstract

--- src/components/StudentForm.js
+++ src/components/StudentForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+//controlled form because input is tied to the state
 
 class StudentForm extends React.Component {
 
@@ -8,16 +9,43 @@ class StudentForm extends React.Component {
     allergies: ''
   }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+    //name: event.target.value
+    //bio: event.target.value
+    //allergies: event.target.value
+  }
+
+  //this.handleChange actually automatically passes event into the function so I don't have to pass it in below
+
   render() {
     return (
       <div>
         <form>
           <label>Add Student </label>
-          <input type="text" placeholder="Name" value={this.state.name}/>
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
           <label>Student Bio </label>
-          <input type="text" placeholder="Bio" value={this.state.bio}/>
+            <input
+              type="text"
+              placeholder="Bio"
+              name="bio"
+              value={this.state.bio}
+            />
           <label>Any Allergies? </label>
-          <input type="text" placeholder="Any Allergies?" value={this.state.allergies}/>
+            <input
+              type="text"
+              placeholder="Any Allergies?"
+              name="allergies"
+              value={this.state.allergies}
+            />
           <label> Payment Received? </label>
           <input type="radio"/>
         </form>
