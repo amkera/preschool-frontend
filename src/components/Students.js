192a3451483139ea6bc89462c243191ef80df81d
amkera <60901491+amkera@users.noreply.github.com>
2020-11-16 21:08:24 -0500

    minor html changes

--- src/components/Students.js
+++ src/components/Students.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 const Students = (props) => {
 
+  // return (
+  //   <div>
+  //     {props.students.map(student => <p key={student.id}>Name: {student.name} Bio: {student.bio} Allergies: {student.allergies} </p>)}
+  //   </div>
+  // )
+
   return (
     <div>
-      {props.students.map(student => <p key={student.id}>Name: {student.name} Bio: {student.bio} Allergies: {student.allergies} </p>)}
+      {props.students.map(student => (
+        <li key={student.id}>
+          <h3> Name: {student.name} </h3>
+          <p> Bio: {student.bio} </p>
+          <p> Allergies: {student.allergies} </p>
+        </li>
+      ))}
     </div>
   )
 
