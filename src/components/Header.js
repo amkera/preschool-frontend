5ed4c50f2009927a92b3b1983b0c6fe1182cf91a
amkera <60901491+amkera@users.noreply.github.com>
2020-11-22 21:38:30 -0500

    adding header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,8 @@ const Header = () => {
 
   return (
     <div class="header">
-      <Link class="link-to-all-students" to="/students"> <button class="header">All Students</button></Link>
-      <Link class="link-to-new-student-form" to="/students/new"> <button class="header">New Student</button></Link>
+      <Link to="/students"> <button class="header">All Students</button></Link>
+      <Link to="/students/new"> <button class="header">New Student</button></Link>
 
     </div>
   )
