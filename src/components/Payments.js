cf0665f045413872c2860e6db91b9469868ded6f
amkera <60901491+amkera@users.noreply.github.com>
2020-11-23 13:18:00 -0500

    fixed date

--- src/components/Payments.js
+++ src/components/Payments.js
@@ -23,7 +23,7 @@ const Payments = (props) => {
 
         <h5 key={payment.id}>
           <p>Amount: ${payment.amount} </p>
-          <p>Date: {payment.date} </p>
+          <p>Date: {payment.date ? payment.date.slice(0,10) : null} </p>
 
           <p>Paid: {payment.paid === true ? "Yes" : "No"} </p>
           <button onClick={() => handleDelete(payment)}>Delete</button>
